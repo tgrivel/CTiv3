48070050f6e1ed6aae873505ec8a80cc722f89c5
TGrivel <tgvl@cbs.nl>
2018-10-10 22:30:05 +0200

    div uitbreidingen

--- routes.py
+++ routes.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/matrix")
+def matrix():
+    return render_template("matrix.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
