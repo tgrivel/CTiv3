eef4e6d3b86b419bb9e268561f429a6c0b6e4021
TGrivel <tgvl@cbs.nl>
2019-01-20 08:16:00 +0100

    voorbeeld leveringen toegevoegd voor 2017. levering en schermen gesplitst naar 5 schermen ipv 3. alle 5 schermen gevuld

--- Verwerking/inlezen.py
+++ Verwerking/inlezen.py
@@ -3,16 +3,13 @@ from collections import OrderedDict
 import urllib.request
 from flask_table import Table, Col
 
+datalastenbaten = {}
 
 HTTP_OKAY = 200
 
 
 def laad_json_bestand(bestand):
-    """Laad een json bestand.
-
-    Het bestand kan gecodeerd zijn als utf-8 of als cp1252.
-    """
-
+    """Laad een json bestand. Het bestand kan gecodeerd zijn als utf-8 of als cp1252. """
     newfile = bestand.read()
     try:
         data = json.loads(newfile.decode('utf-8'), object_pairs_hook=OrderedDict)
@@ -27,15 +24,14 @@ def ophalen_sjabloon(meta):
     metadata = []
     ovlaag = meta['overheidslaag']
     boekjaar = meta['boekjaar']
-    # naam_sjabloon = 'C:\Theo\werk\CTiv3\sjablonen\iv3Codes' + ovlaag + boekjaar + '.json'
     url = "https://raw.github.com/tgrivel/iv3_modellen/master/" + "iv3Codes" + ovlaag + boekjaar + ".json"
     print(url)
     webUrl = urllib.request.urlopen(url)
     if (webUrl.getcode() == HTTP_OKAY):
         # bestand = webUrl.read()
-        metadata = laad_json_bestand(webUrl)
-
-    return metadata
+        inhoud_bestand = laad_json_bestand(webUrl)
+        meta_data = inhoud_bestand['metadata']
+    return inhoud_bestand
 
 
 def verwerken(data):
@@ -50,3 +46,38 @@ def verwerken(data):
     #     aantal += 1
     # print('totaal ' + str(aantal) + ' ingelezen')
     return meta
+
+def indikken_data(data):
+    global datalastenbaten
+    print('in indikken')
+    for rec in data:
+        try:
+            kant = rec['rekeningkant']
+            bedrag = rec['bedrag']
+            # bedrag = float(rec['bedrag'].replace(',', '.'))
+            if kant == 'lasten' or kant == 'baten':
+                cat = rec['categorie']
+                taakv = rec['taakveld']
+                code = (kant, taakv, cat)
+            elif kant == 'balans_baten' or kant == 'balans_lasten':
+                cat = rec['categorie']
+                bal_code = rec['balanscode']
+                code = (kant, bal_code, cat)
+            else:
+                 stand_per = rec['standper']
+                 bal_code = rec['balanscode']
+                 code = ('balans_standen', bal_code, stand_per)
+
+            if code in datalastenbaten:
+                datalastenbaten[code] += bedrag
+            else:
+                datalastenbaten[code] = bedrag
+        except:
+            print('Fout in indikken ')
+            for k,v in rec.items():
+                print (k,v)
+    # print('export: ')
+    # for k, v in datalastenbaten.items():
+    #     print(k,v)
+    return datalastenbaten
+
--- routes.py
+++ routes.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request
 
-from Verwerking.inlezen import laad_json_bestand, ophalen_sjabloon
-from Verwerking.maak_matrix import data, header
+from Verwerking.inlezen import laad_json_bestand, ophalen_sjabloon, indikken_data
+from Verwerking.maak_matrix import maak_tabel, maak_lijst_koppen
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET','POST'])
 def index():
     if request.method == 'POST':
-        return matrix(bestandsnaam=request.files['file'])
+        try:
+            print("proberen naar matrix te gaan")
+            return matrix(bestandsnaam=request.files['file'])
+        except:
+            return render_template("index.html")
     elif request.method == 'GET':
         return render_template("index.html")
 
@@ -16,19 +20,39 @@ def index():
 def matrix(bestandsnaam):
     """Laad de pagina met een overzicht."""
     if bestandsnaam:
-        data2 = laad_json_bestand(bestandsnaam)
-        metadata = data2['metadata']
+        complete_upload = laad_json_bestand(bestandsnaam)
+        metadata = complete_upload['metadata']
         sjabloon = ophalen_sjabloon(metadata)
-        data1 = data # dit is nep-data, niet ingelezen
+        sjabloon_meta = sjabloon['metadata']
+        compacte_data = indikken_data(complete_upload['waarden'])
+        print('stap 1')
+        lasten_header = {'kolkop': maak_lijst_koppen(sjabloon,"LastenCategorien")}
+        baten_header = {'kolkop': maak_lijst_koppen(sjabloon,"BatenCategorien")}
+        balans_header = {'kolkop': maak_lijst_koppen(sjabloon,"BalansDatums")}
+        rekening_rijen = {'rijkop': maak_lijst_koppen(sjabloon,'Taakvelden')}
+        balans_rijen = {'rijkop': maak_lijst_koppen(sjabloon,'Balanscodes')}
+        print('stap 2')
+        lasten_tabel = maak_tabel(compacte_data, 'lasten',lasten_header, rekening_rijen)
+        baten_tabel = maak_tabel(compacte_data, 'baten', baten_header, rekening_rijen)
+        balans_lasten_tabel = maak_tabel(compacte_data, 'balans_lasten', lasten_header, balans_rijen)
+        balans_baten_tabel = maak_tabel(compacte_data, 'balans_baten', baten_header, balans_rijen)
+        balans_standen_tabel = maak_tabel(compacte_data, 'balans_standen', balans_header, balans_rijen)
+        # data1 = data # dit is nep-data, niet ingelezen
+        print('stap 3')
 
-        # TODO Dit moet nog wat mooier
         params = {
-            'data': data2,
-            'x': data1,
-            'h': header,
+            'lasten_header': lasten_header,
+            'baten_header': baten_header,
+            'balans_header': balans_header,
+            'lasten_tabel':lasten_tabel,
+            'baten_tabel': baten_tabel,
+            'balans_lasten_tabel': balans_lasten_tabel,
+            'balans_baten_tabel': balans_baten_tabel,
+            'balans_standen_tabel': balans_standen_tabel,
+            'data': complete_upload,
             'meta': metadata,
-            'sjabloon': sjabloon,
-            # 'taakvelden': hoofdtaakvelden
+            'sjabloon': sjabloon_meta,
+
         }
 
     return render_template("matrix.html", **params)
