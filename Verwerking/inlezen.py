21a6e6cc048955274da8799009ad83dbb6295ded
lverweijen <lverweijen>
2019-01-30 14:28:22 +0100

    Schoon kleine dingen op

--- Verwerking/inlezen.py
+++ Verwerking/inlezen.py
@@ -1,11 +1,13 @@
+import http
 import json
 from collections import OrderedDict
 import urllib.request
+import logging
 from flask_table import Table, Col
 
 datalastenbaten = {}
 
-HTTP_OKAY = 200
+_logger = logging.getLogger(__file__)
 
 
 def laad_json_bestand(bestand):
@@ -13,10 +15,10 @@ def laad_json_bestand(bestand):
     newfile = bestand.read()
     try:
         data = json.loads(newfile.decode('utf-8'), object_pairs_hook=OrderedDict)
-        print('verwerken als utf8')
+        _logger.info('verwerken als utf8')
     except UnicodeDecodeError:
         data = json.loads(newfile.decode('cp1251'), object_pairs_hook=OrderedDict)
-        print('verwerken als cp1251')
+        _logger.info('verwerken als cp1251')
     return data
 
 
@@ -25,24 +27,27 @@ def ophalen_sjabloon(meta):
     ovlaag = meta['overheidslaag']
     boekjaar = meta['boekjaar']
     url = "https://raw.github.com/tgrivel/iv3_modellen/master/" + "iv3Codes" + ovlaag + boekjaar + ".json"
-    print(url)
+    _logger.info("Sjabloon opgehaald van %s", url)
     webUrl = urllib.request.urlopen(url)
-    if (webUrl.getcode() == HTTP_OKAY):
-        # bestand = webUrl.read()
+    if (webUrl.getcode() == http.HTTPStatus.OK):
         inhoud_bestand = laad_json_bestand(webUrl)
-        meta_data = inhoud_bestand['metadata']
 
     return inhoud_bestand
 
 
 
 def verwerken(data):
+    """Wat doet deze functie?
+
+    Iets optellen en meta teruggeven?
+
+    """
     meta = data["metadata"]
     records  = data["waarden"]
     totaal = 0
     for record in records:
         totaal += int(record['bedrag'])
-    print(totaal)
+    _logger.info("Aantal records %d", totaal)
     # aantal = 0
     # for row in data:
     #     aantal += 1
@@ -51,8 +56,13 @@ def verwerken(data):
 
 
 def indikken_data(data):
-    global datalastenbaten
-    print('in indikken')
+    """"Wat is indikken??
+
+    data zijn alleen de waarden
+    """
+    datalastenbaten = {}
+
+    _logger.info('in indikken')
     for rec in data:
         try:
             kant = rec['rekeningkant']
