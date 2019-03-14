3df4d63acf46abc07e9d76d9f3a60dc50da61e2d
lverweijen <lverweijen>
2019-03-14 14:32:01 +0100

    Zorg voor foutafhandeling
    - Propageer foutmeldingen naar GUI
    - Zorg voor correcte foutafhandeling wanneer de json-parser faalt
    - Zorg voor correcte foutafhandeling wanneer de latin-1-fallback voor de encoding niet werkt.

--- applicatie/logic/inlezen.py
+++ applicatie/logic/inlezen.py
@@ -2,6 +2,8 @@ import http
 import json
 from collections import OrderedDict
 import urllib.request
+from json import JSONDecodeError
+from typing import Iterable
 from urllib.error import URLError, HTTPError
 import logging
 from flask_table import Table, Col
@@ -14,16 +16,45 @@ _logger = logging.getLogger(__file__)
 def laad_json_bestand(bestand):
     """Laad een json bestand. Het bestand kan gecodeerd zijn als utf-8 of als cp1252."""
     newfile = bestand.read()
-    try:
-        data = json.loads(newfile.decode('utf-8'), object_pairs_hook=OrderedDict)
-        _logger.info('verwerken als utf8')
-    except UnicodeDecodeError:
-        data = json.loads(newfile.decode('cp1251'), object_pairs_hook=OrderedDict)
-        _logger.info('verwerken als cp1251')
-    return data
+    data = None
+    foutmeldingen = []
+
+    # Vind een geschikte encoding
+    encodings = ['utf8', 'cp1252']
+
+    for encoding in encodings:
+        try:
+            data = json.loads(newfile.decode(encoding), object_pairs_hook=OrderedDict)
+            _logger.info(f'verwerken als {encoding}')
+            break
+        except UnicodeDecodeError:
+            continue  # Probeer de volgende
+        except JSONDecodeError as e:
+            foutmeldingen.append(f"{bestand} is geen JSON-bestand")
+            break
+    else:
+        foutmeldingen.append('Het bestand kon niet gelezen worden. '
+                             'Geef een geschikt json-bestand.')
+
+    return data, foutmeldingen
+
+
+def ophalen_databestand(jsonbestand):
+    """Haal jsonbestand op.
+
+    Geef json-bestand terug of een lijst met foutmeldingen.
+    """
+
+    # json bestand inlezen
+    complete_upload, foutmeldingen = laad_json_bestand(jsonbestand)
+
+    # TODO Valideren met schema en foutmeldingen aanvullen
+
+    return complete_upload, foutmeldingen
 
 
 def ophalen_definitiebestand(meta):
+    foutmeldingen = []
     ovlaag = meta['overheidslaag']
     boekjaar = meta['boekjaar']
     bestandsnaam = 'iv3_definities_' + ovlaag + '_' + boekjaar + '.json'
@@ -40,13 +71,16 @@ def ophalen_definitiebestand(meta):
         errorcode = e.code
     if errorcode == 0:
         if webUrl.getcode() == http.HTTPStatus.OK:
-            inhoud_bestand = laad_json_bestand(webUrl)
+            inhoud_bestand, foutmeldingen_json = laad_json_bestand(webUrl)
+            foutmeldingen.extend(foutmeldingen_json)
             _logger.info("Sjabloon opgehaald van %s", url)
     else:
         errormessage = 'Fout bij ophalen definitiebestand {0} (foutcode #{1})'.format(bestandsnaam, errorcode)
+        foutmeldingen.append(errormessage)
         _logger.info("Fout bij ophalen Iv3 definitiebestand")
 
-    resultaat = [inhoud_bestand, errormessage]
-    return resultaat
+    # TODO Valideren met schema en foutmeldingen aanvullen
+
+    return inhoud_bestand, foutmeldingen
 
 
