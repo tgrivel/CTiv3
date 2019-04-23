f14e7d159fbd80d4f64f8a7a9b393309fd18f886
lverweijen <lverweijen>
2019-04-23 13:40:41 +0200

    Minder ingewikkelde manier om omschrijvingen te tonen in tabel

--- applicatie/logic/draaitabel.py
+++ applicatie/logic/draaitabel.py
@@ -7,7 +7,7 @@ WAARDE_KOLOM = 'bedrag'
 
 
 class DraaiTabel:
-   def __init__(self, data, rij_naam, kolom_naam):
+   def __init__(self, data, rij_naam, kolom_naam, rij_omschrijvingen, kolom_omschrijvingen):
        indices, self.tabel = pivot_table(data, aggregeer_kolommen=[rij_naam, kolom_naam],
                                          waarde_kolom=WAARDE_KOLOM)
        self.rij_naam, self.kolom_naam = rij_naam, kolom_naam
@@ -17,24 +17,27 @@ class DraaiTabel:
        self.kolommen = sorted(self.kolommen)
        self.data = data
 
+       # Onthoud omschrijvingen
+       self.rij_omschrijvingen = rij_omschrijvingen
+       self.kolom_omschrijvingen = kolom_omschrijvingen
+
    def __getitem__(self, args):
        """Geef een specifieke waarde uit de draaitabel terug."""
        return self.tabel[args]
 
    def geef_detail_kolommen(self):
         unieke_labels = reduce(set.union, [rij.keys() for rij in self.data], set())
-        r = self.rij_naam
-        k = self.kolom_naam
-        rs = r[:-1] if r[-1:] == ':' else r
-        ks = k[:-1] if k[-1:] == ':' else k
-        rsub = 'sub_' + rs
-        ksub = 'sub_' + ks
-        relevante_labels = unieke_labels - {r, k, rs, ks, rsub, ksub, WAARDE_KOLOM}
+        rsub = 'sub_' + self.rij_naam
+        ksub = 'sub_' + self.kolom_naam
+        relevante_labels = unieke_labels - {self.rij_naam, self.kolom_naam, rsub, ksub, WAARDE_KOLOM}
 
         # Sort labels alphabetically but always put value last
+        # TODO Waarom dit onderscheid?
         if ksub in unieke_labels:
-            details = [rs] + [ks] + [rsub] + [ksub] + sorted(relevante_labels, key=str.lower) + [WAARDE_KOLOM]
+            details = ([self.rij_naam] + [self.kolom_naam] + [rsub] + [ksub]
+                       + sorted(relevante_labels, key=str.lower) + [WAARDE_KOLOM])
         else:
-            details = [rs] + [ks] + [rsub] + sorted(relevante_labels, key=str.lower) + [WAARDE_KOLOM]
+            details = ([self.rij_naam] + [self.kolom_naam] + [rsub]
+                       + sorted(relevante_labels, key=str.lower) + [WAARDE_KOLOM])
 
         return details
