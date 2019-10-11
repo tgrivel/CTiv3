378acc90ed199353977763c5a5f0fc1179f30d4e
lverweijen <lverweijen>
2019-10-11 14:19:57 +0200

    Dit voegt de externe bibliotheken toe aan de repo

---
+++ applicatie/static/js/extern/download_bibliotheken.ps1
@@ -0,0 +1,16 @@
+﻿    # Download javascript bibliotheken
+    
+    $bieb = (
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.1.2/papaparse.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/chosen/1.4.2/chosen.jquery.js",
+    "https://cdn.plot.ly/plotly-basic-latest.min.js",
+    "https://code.jquery.com/jquery-3.3.1.min.js")
+
+    foreach ($boek in $bieb) {
+        $out = $boek -split '/'
+        Invoke-WebRequest $best -UseBasicParsing -OutFile $out[-1]
+    }
