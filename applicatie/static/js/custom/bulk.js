a15f088ce086617e4b90a27c8390a9067f9edd92
Vincent Ohm <vs.ohm@cbs.nl>
2020-08-27 16:20:28 +0200

    form controls reactief maken

--- applicatie/static/js/custom/bulk.js
+++ applicatie/static/js/custom/bulk.js
@@ -1,4 +1,35 @@
 
+function initialiseer_form_controls_bulkcorrecties() {
+
+    let form_bulk = $("#Form_bulkcorrecties");
+    if (form_bulk.length){
+        //alert("Form gevonden");
+
+        let bulkcorrecties = form_bulk.find("div[id^='bulkcorr_']");
+
+        $.each(bulkcorrecties, function(key, corr) {
+
+            // voeg events toe aan form-controls:
+            // change(): type, rij_soort, kolom_soort
+            // click():  reset
+
+            $(this).find("#type").change(function() {
+                form_control_changed_type($(corr)); });
+
+            $(this).find("#rij_soort").change(function() {
+                form_control_changed_rij($(corr)); });
+
+            $(this).find("#kolom_soort").change(function() {
+                form_control_changed_kol($(corr)); });
+
+            // button: click event
+            $(this).find("#reset").click(function() {
+                form_control_reset_row($(corr)); });
+       });
+    }
+};
+
+
 function knop_werkt_niet() {
 
     alert("Deze knop werkt nog niet.");
@@ -6,7 +37,16 @@ function knop_werkt_niet() {
 
 function formulier_bulkcorrecties_leegmaken() {
 
-    document.getElementById("Form_bulkcorrecties").reset();
+    let form_bulk = $("#Form_bulkcorrecties");
+    if (form_bulk.length){
+
+        let bulkcorrecties = form_bulk.find("div[id^='bulkcorr_']");
+
+        $.each(bulkcorrecties, function(key, corr) {
+
+            form_control_reset_row($(corr));
+        });
+    }
 };
 
 function form_control_changed_type(corr) {
@@ -21,22 +61,6 @@ function form_control_changed_type(corr) {
 
     alert("Geselecteerd: " + type_selected + " ; " + rij_selected + " ; " + kol_selected);
 
-    // reset selectie van rij_soort
-    let x = corr.find("#rij_soort").closest("select");
-    x.val(x.data("default-value"));
-
-    // reset selectie van kolom_soort
-    x = corr.find("#kolom_soort").closest("select");
-    x.val(x.data("default-value"));
-
-    // reset selectie van bedrag
-    x = corr.find("#bedrag");
-    x.val("0");
-
-    // reset selectie van bedrag
-    x = corr.find("#opmerking");
-    x.val("Bulkcorrectie Ocido");
-
 };
 
 function form_control_changed_rij(corr) {
@@ -48,4 +72,43 @@ function form_control_changed_rij(corr) {
 function form_control_changed_kol(corr) {
 
     alert("Kolom - Gewijzigd: " + corr.attr("id"));
+};
+
+
+function form_control_reset_row(corr) {
+    // code voor de reset knop per correctieregel
+
+    // alert("Regel: " + corr.attr("id"));
+
+    // reset selectie van type
+    let x = corr.find("#type").closest("select");
+    x.val(x.data("default-value"));
+
+    // reset selectie van rij_soort
+    x = corr.find("#rij_soort").closest("select");
+    x.val(x.data("default-value"));
+
+    // reset selectie van rij_code
+    x = corr.find("#rij_code").closest("select");
+    x.val(x.data("default-value"));
+
+    // reset selectie van kolom_soort
+    x = corr.find("#kolom_soort").closest("select");
+    x.val(x.data("default-value"));
+
+    // reset selectie van kolom_code
+    x = corr.find("#kolom_code").closest("select");
+    x.val(x.data("default-value"));
+
+    // reset bedrag
+    x = corr.find("#bedrag");
+    x.val("0");
+
+    // reset opmerking
+    x = corr.find("#opmerking");
+    x.val("Bulkcorrectie Ocido");
+
+    // reset check (hidden)
+    x = corr.find("#check");
+    x.val(false);
 };
