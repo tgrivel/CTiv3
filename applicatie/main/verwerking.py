9ab4615f6a2bbbd23d35677ea2dbcb58d79a776a
J. Gelderblom <jaapgelderblom@gmail.com>
2020-09-02 11:50:47 +0200

    Correcie.

--- applicatie/main/verwerking.py
+++ applicatie/main/verwerking.py
@@ -38,11 +38,13 @@ class Verwerking(object):
             self.contact = self.data_bestand['contact']
             self._ophalen_definitiebestand()
 
+        if not self.fouten:
             # Controle databestand met definitiebestand
             self.fouten = controle_met_defbestand(self.data_bestand, self.definitie_bestand)
 
-            # TODO Aggregeren via externe API
-            # TODO Controle moet uitgevoerd worden in een externe API
+        # TODO Aggregeren via externe API
+        # TODO Controle moet uitgevoerd worden in een externe API
+        if not self.fouten:
             self.controle_resultaten = []
 
             # Zoek omschrijvingen bij de codes zodat we deze in de tabel kunnen tonen
