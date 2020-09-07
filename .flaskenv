4c5de43534f9660125a8b157515ad52557e284d4
Vincent Ohm <vs.ohm@cbs.nl>
2020-09-07 19:48:05 +0200

    verwerken opmerkingen code review

--- .flaskenv
+++ .flaskenv
@@ -1,6 +1,8 @@
 FLASK_APP=applicatie
-FLASK_ENV=development       # voor development is DEBUG True (default)
+FLASK_ENV=development       # voor development is de default DEBUG = True
 
-# FLASK_DEBUG=False           # zet expliciet op False indien je Flask niet in DEBUG mode wil runnen
+# zet expliciet op False indien je Flask niet in DEBUG mode wil runnen:
+# FLASK_DEBUG=False
 
-# FLASK_ENV=debugjavascript   # kies deze env indien je javascript code wil debuggen
+# kies deze env indien je javascript code wil debuggen:
+# FLASK_ENV=debugjavascript
--- applicatie/static/js/custom/bulk.js
+++ applicatie/static/js/custom/bulk.js
@@ -7,7 +7,7 @@ function initialiseer_form_controls_bulkcorrecties() {
 
     if (!form_bulk.length){
 
-        alert("Formulier bulkcorrecties niet gevonden.");
+        alert("Formulier " + FORMULIER_BULK + " niet gevonden.");
 
     } else {
 
@@ -15,15 +15,15 @@ function initialiseer_form_controls_bulkcorrecties() {
 
         $.each(bulkcorrecties, function(key, corr) {
 
-            // voeg events toe:
-            // change() voor form_controls Type
-            // click() voor button Reset
-
+            // change() event toevoegen aan form control 'Type'
+            // zie https://api.jquery.com/change/
             $(this).find("#type").change(function() {
-                form_control_changed_type($(corr)); });
+                form_control_type_gewijzigd($(corr)); });
 
+            // click() event toevoegen aan button 'Reset'
+            // zie https://api.jquery.com/click/
             $(this).find("#reset").click(function() {
-                form_control_reset_row($(corr)); });
+                form_control_reset_rij($(corr)); });
        });
     }
 };
@@ -35,9 +35,8 @@ function bulkcorrecties_verwerken() {
     tekst = tekst + "Zo ja,  klik op OK\r\n"
     tekst = tekst + "Zo nee, klik op Annuleren (Cancel)"
 
-
     if (confirm(tekst)) {
-        console.log('Status: ' + DEBUG_STATUS);
+        console.log('Debug status: ' + DEBUG_STATUS);
     }
 };
 
@@ -52,49 +51,51 @@ function formulier_bulkcorrecties_leegmaken() {
 
         $.each(bulkcorrecties, function(key, corr) {
             // reset de correctieregel
-            form_control_reset_row($(corr));
+            form_control_reset_rij($(corr));
         });
     }
 };
 
 
-function form_control_changed_type(corr) {
+function form_control_type_gewijzigd(corr) {
     /* form_control Type is gewijzigd
        overige form-controls updaten */
 
-    // voor debugging:
-    // alert("Gewijzigde correctieregel: " + corr.attr("id"));
+    if (DEBUG_STATUS){
+        console.log("Gewijzigde correctieregel: " + corr.attr("id"));
+    }
 
     // geselecteerde waarde voor type
-    let type_selected = corr.find("#type").children("option:selected").val();
+    let type_geselecteerd = corr.find("#type").children("option:selected").val();
 
-    // update selectie opties voor form_controls
-    form_control_update_options($(corr), "rij_soort", type_selected, true);
-    form_control_update_options($(corr), "rij_code", type_selected, false);
-    form_control_update_options($(corr), "kolom_soort", type_selected, true);
-    form_control_update_options($(corr), "kolom_code", type_selected, false);
+    // update selectie opties voor form_controls van een correctieregel
+    form_control_update_opties($(corr), "rij_soort", type_geselecteerd, true);
+    form_control_update_opties($(corr), "rij_code", type_geselecteerd, false);
+    form_control_update_opties($(corr), "kolom_soort", type_geselecteerd, true);
+    form_control_update_opties($(corr), "kolom_code", type_geselecteerd, false);
 
 };
 
-function form_control_update_options(corr, kolom, type_selected, select) {
-    /* update of delete de opties voor een form_control */
+
+function form_control_update_opties(corr, kolom, type_geselecteerd, select) {
+    /* update de selectie opties voor een form control */
 
     let form_bulk = $("#" + FORMULIER_BULK);
 
-    let arr_options;
+    let arr_opties;
+    arr_opties = [];
 
-    arr_options = [];
-    if (type_selected != "") {
+    if (type_geselecteerd != "") {
         // haal string met opties op
-        let str_options = form_bulk.find("#" + kolom + "_opties").val().replace(/'/g, '"');
-        // omzetten naar array met opties
-        arr_options = $.parseJSON(str_options)[type_selected];
+        let str_opties = form_bulk.find("#" + kolom + "_opties").val().replace(/'/g, '"');
+        // omzetten naar een array met opties
+        arr_opties = $.parseJSON(str_opties)[type_geselecteerd];
     }
 
-    // opties updaten voor form_control
+    // selecteer de form control
     let form_control = corr.find("#" + kolom);
 
-    // oude opties verwijderen
+    // oude selectie opties verwijderen
     form_control.find("option").each(function() {
         if (this.value){
             $(this).remove();
@@ -104,9 +105,9 @@ function form_control_update_options(corr, kolom, type_selected, select) {
     });
 
     // nieuwe opties toevoegen uit array
-    form_control_select = form_control.closest("select");
-    for (i = 0; i < arr_options.length; i++) {
-        let opt = arr_options[i];
+    let form_control_select = form_control.closest("select");
+    for (i = 0; i < arr_opties.length; i++) {
+        let opt = arr_opties[i];
         let el = document.createElement("option");
         el.textContent = opt;
         el.value = opt;
@@ -117,51 +118,38 @@ function form_control_update_options(corr, kolom, type_selected, select) {
         form_control_select.append(el);
     }
 
-    // voor debugging:
-    // let selected = form_control.children("option:selected").val();
-    // console.log("selected option: " + selected);
+    if (DEBUG_STATUS){
+        let selected = form_control.children("option:selected").val();
+        console.log("Geselecteerde optie: " + selected);
+    }
 
 };
 
 
-function form_control_reset_row(corr) {
-    /* reset de correctieregel */
+function form_control_reset_rij(corr) {
+    /* reset een correctieregel */
 
-    // verwijder alle opties voor de form_controls
-    form_control_update_options($(corr), "rij_soort", "", false);
-    form_control_update_options($(corr), "rij_code", "", false);
-    form_control_update_options($(corr), "kolom_soort", "", false);
-    form_control_update_options($(corr), "kolom_code", "", false);
+    // verwijder selectie opties voor de form_controls
+    form_control_update_opties($(corr), "rij_soort", "", false);
+    form_control_update_opties($(corr), "rij_code", "", false);
+    form_control_update_opties($(corr), "kolom_soort", "", false);
+    form_control_update_opties($(corr), "kolom_code", "", false);
 
-    // reset waarde van type
-    let x = corr.find("#type").closest("select");
-    x.val(x.data("default-value"));
+    // reset alle kolommen naar default waardes
+    arr = ["type", "rij_soort", "rij_code", "kolom_soort", "kolom_code"]
+    for (i = 0; i < arr.length; i++) {
+        let foo = corr.find("#" + arr[i]).closest("select");
+        foo.val(foo.data("default-value"));
+    }
 
-    // reset waarde van rij_soort
-    x = corr.find("#rij_soort").closest("select");
-    x.val(x.data("default-value"));
+    let foo;
 
-    // reset waarde van rij_code
-    x = corr.find("#rij_code").closest("select");
-    x.val(x.data("default-value"));
+    foo = corr.find("#bedrag");
+    foo.val("0");
 
-    // reset waarde van kolom_soort
-    x = corr.find("#kolom_soort").closest("select");
-    x.val(x.data("default-value"));
+    foo = corr.find("#opmerking");
+    foo.val("Bulkcorrectie Ocido");
 
-    // reset waarde van kolom_code
-    x = corr.find("#kolom_code").closest("select");
-    x.val(x.data("default-value"));
-
-    // reset bedrag
-    x = corr.find("#bedrag");
-    x.val("0");
-
-    // reset opmerking
-    x = corr.find("#opmerking");
-    x.val("Bulkcorrectie Ocido");
-
-    // reset check (hidden)
-    x = corr.find("#check");
-    x.val(false);
+    foo = corr.find("#check");
+    foo.val(false);
 };
