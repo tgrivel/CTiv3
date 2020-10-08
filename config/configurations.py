0f8796afc62fb25b094628bfc87d4d23aaf7e970
J. Gelderblom <jaapgelderblom@gmail.com>
2020-10-08 15:43:00 +0200

    Werkende versie die geen externe API call gebruikt.

--- config/configurations.py
+++ config/configurations.py
@@ -11,7 +11,7 @@ IV3_SCHEMA_FILE = "iv3_data_schema_v{}.json"
 IV3_DEF_FILE = "iv3_definities_{}_{}.json"
 
 # Bepaalt of json bestand-controle met definitiebestand extern met API call gebeurt of niet.
-EXTERNE_DEF_BESTAND_CONTROLE = True
+EXTERNE_DEF_BESTAND_CONTROLE = False
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
