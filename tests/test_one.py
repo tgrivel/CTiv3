f1b2aa2bb0335789c545e5763676441e7fd41ded
J. Gelderblom <jaapgelderblom@gmail.com>
2020-10-15 12:07:59 +0200

    Reorganiseerd de testen.

--- tests/test_one.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class Test_test_test(unittest.TestCase):
-    def test_one_is_one(self):
-        self.assertEqual(1, 1)
-
-    def test_two_is_two(self):
-        self.assertEqual(2, 2)
