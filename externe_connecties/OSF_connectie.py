a2fe6813778960f87f03cacb297851d323993d10
J. Gelderblom <jaapgelderblom@gmail.com>
2020-10-15 17:57:47 +0200

    Maakt OSF aanroep functie ipv class en unittest draait.

--- externe_connecties/OSF_connectie.py
+++ externe_connecties/OSF_connectie.py
@@ -1,54 +1,53 @@
 import requests
 
 
-class OSF_connectie(object):
+def OSF_aanroep(): # json_bestand, api_url):
     """
     Regelt de aanroep naar de externe API van Open State Foundation (OSF).
+    Zend JSON bestand naar OSF API waar controle plaatsvindt en die JSON bestand met fouten element terug stuurt.
     """
-    def __init__(self):
-        # self.respons = []
-        # self.fouten = []
-        pass
-
-    # def geef_fouten(self):
-    #     return self.fouten
-
-    def geef_fouten(self): # json_bestand, api_url):
-        """
-        Zend JSON bestand naar OSF API waar controle plaatsvindt en die JSON bestand met fouten element terug stuurt.
-        """
-        # codefouten = []
+    # codefouten = []
 
 
 
-        # json_bestand =
-        api_url = 'https://cbs.openstate.eu/geef_totalen_uitkomst'
+    # json_bestand =
+    api_url = 'https://cbs.openstate.eu/geef_totalen_uitkomst'
 
 
 
-        # f = open(json_bestand, 'rb')
-        # bestanden = {'file': f}
+    # f = open(json_bestand, 'rb')
+    # bestanden = {'file': f}
 
-        # try:
-        # response = requests.post(api_url, files=bestanden)
-        response = requests.post(api_url)
+    # try:
+    # response = requests.post(api_url, files=bestanden)
+    response = requests.post(api_url)
 
 
 
 
-        response_json_format = response.json()
-        fouten_element = response_json_format["fouten"]
-        print("regel 40 response geeft fouten_element: ", fouten_element)
+    # response_json_format = response.json()
+    # fouten_element = response_json_format["fouten"]
+    # print("regel 40 response geeft fouten_element: ", fouten_element)
 
-        # except FileNotFoundError:
-        #     print("Het geuploade json bestand bestaat niet of kan niet gevonden worden.")
-        #     codefouten.append("Het geuploade json bestand bestaat niet of kan niet gevonden worden.")
-        #     _logger.info("Het geuploade json bestand bestaat niet of kan niet gevonden worden.")
+    # except FileNotFoundError:
+    #     print("Het geuploade json bestand bestaat niet of kan niet gevonden worden.")
+    #     codefouten.append("Het geuploade json bestand bestaat niet of kan niet gevonden worden.")
+    #     _logger.info("Het geuploade json bestand bestaat niet of kan niet gevonden worden.")
 
-        # f.close()
+    # f.close()
 
-        # self.fouten = fouten_element
+    # self.fouten = fouten_element
 
-        # return codefouten
-        # return fouten_element
-        return response_json_format
+    # return codefouten
+    return response
+
+# def geef_fouten(self):
+#     # response = self.geef_response()
+#     #
+#     # response_json_format = response.json()
+#     # fouten_element = response_json_format["fouten"]
+#     #
+#     # # self.fouten = fouten_element
+#     #
+#     # # return codefouten
+#     # return fouten_element
--- unittesten/test_OSF_connectie.py
+++ unittesten/test_OSF_connectie.py
@@ -1,29 +1,25 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
-from externe_connecties.OSF_connectie import OSF_connectie
+from externe_connecties.OSF_connectie import OSF_aanroep
 
 
 class TestOSF_connectie(TestCase):
+    # @patch('externe_connecties.OSF_connectie.geef_fouten')
     @patch('externe_connecties.OSF_connectie.requests.post')
     def test_stuur_bestand_voor_controle(self, mock_post):
         fouten_element_referentie = \
-        {'fouten': {'fout': 11333.0,
-                    'foutcode': 'PF006',
-                    'goed': 50,
-                    'melding': 'Totaal lasten taakvelden is niet gelijk aan totaal baten '
-                               'taakvelden. Het gevonden verschil is 24571.0, terwijl 50 '
-                               'maximaal is toegestaan.',
-                    'omschrijving': 'Controle 6 onvoldoende'}}
-        mock_post.return_value = Mock(ok=True)
+            {'fouten': {'fout': 123, 'foutcode': 'PF006', 'melding': 'een melding', 'omschrijving': 'een omschrijving'}}
+        mock_post.return_value = Mock()
         mock_post.return_value.json.return_value = fouten_element_referentie
 
         # json_bestand = fake_bestand
         # api_url = mock_server
 
-        osf_connectie = OSF_connectie()
+        # osf_connectie = OSF_connectie()
         # fouten = osf_connectie.stuur_bestand_voor_controle(json_bestand, api_url)
-        fouten = osf_connectie.geef_fouten()
+        fouten = OSF_aanroep()
 
-        self.assertEqual(fouten_element_referentie, fouten, 'De lijsten zijn niet gelijk.')
+        # self.assertTrue(mc.geef_response.called)
+        self.assertEqual(fouten_element_referentie, fouten.json(), 'De lijsten zijn niet gelijk.')
         # self.assertTrue(False)
